a61940562656bb7f280257ecb05f9e45a4dbf1ab
shaikhjazib <119857512+shaikhjazib@users.noreply.github.com>
2024-02-04 23:43:15 +0500

    Add files via upload

---
+++ Gallery_.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication2
+{
+    public class Gallery_
+    {
+        [Key]
+
+        public int Gallery_Id { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Required]
+        public IFormFile Photo { get; set; }
+    }
+}
---
+++ Product_.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication2
+{
+    public class Product_
+    {
+        internal string image;
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+
+        public String  Name { get; set; }
+
+        [Required]
+
+        public int Price { get; set; }
+
+        [Required]
+        public IFormFile Photo { get; set; }
+    }
+}
